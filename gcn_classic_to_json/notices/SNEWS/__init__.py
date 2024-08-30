db820d07887d109f7be8c5888700c97475da0936
Athish Thiruvengadam <athisht2@illinois.edu>
2024-08-30 14:37:54 -0500

    removed comments and added detector_quality field

--- gcn_classic_to_json/notices/SNEWS/__init__.py
+++ gcn_classic_to_json/notices/SNEWS/__init__.py
@@ -2,36 +2,8 @@ import numpy as np
 
 from ... import utils
 
-misc_descriptions = {
-    0: "Super-K contributed to this notice.",
-    1: "Super-K quality factor is possible.",
-    2: "Super-K quality factor is good.",
-    3: "Super-K quality factor is override.",
-    4: "LVD contributed to this notice.",
-    5: "LVD quality factor is possible.",
-    6: "LVD quality factor is good.",
-    7: "LVD quality factor is override.",
-    8: "IceCube contributed to this notice.",
-    9: "IceCube quality factor is possible.",
-    10: "IceCube quality factor is good.",
-    11: "IceCube quality factor is override.",
-    12: "KamLAND contributed to this notice.",
-    13: "KamLAND quality factor is possible.",
-    14: "KamLAND quality factor is good.",
-    15: "KamLAND quality factor is override.",
-    16: "Borexino contributed to this notice.",
-    17: "Borexino quality factor is possible.",
-    18: "Borexino quality factor is good.",
-    19: "Borexino quality factor is override.",
-    20: "Daya Bay contributed to this notice.",
-    21: "Daya Bay quality factor is possible.",
-    22: "Daya Bay quality factor is good.",
-    23: "Daya Bay quality factor is override.",
-    24: "HALO contributed to this notice.",
-    25: "HALO quality factor is possible.",
-    26: "HALO quality factor is good.",
-    27: "HALO quality factor is override.",
-}
+detectors = ["Super-K", "LVD", "IceCube", "KamLAND", "Borexino", "Daya Bay", "HALO"]
+detector_quality_opts = ["did not contribute", "possible", "good", "override"]
 
 
 def parse(bin):
@@ -40,41 +12,35 @@ def parse(bin):
     bin[20:39]  # Spare. According to Docs: '76 bytes for the future'
 
     trig_id_bits = np.flip(np.unpackbits(bin[18:19].view(dtype="u1")))
+    # trig_id_bits[0] contains if the detection in individual or coincidence.
+    # I've intentially omitted this since it can be infered from detector quality
 
-    comments = ""
-    comments += (
-        "This is an Individual detection.\n"
-        if trig_id_bits[0] == 1
-        else "This is an Coincidence detection.\n"
-    )
-    comments += (
-        "This is a test notice.\n"
-        if trig_id_bits[1] == 1
-        else "This is a real notice.\n"
-    )
-    comments += (
-        "RA/Dec is undefined.\n" if trig_id_bits[2] == 1 else "RA/Dec is defined.\n"
-    )
-    if trig_id_bits[5] == 1:
-        comments += "This is definitely not a core-collapse Supernova."
+    ra, dec = bin[7] * 1e-4, bin[8] * 1e-4
 
     misc_bits = np.flip(np.unpackbits(bin[19:20].view(dtype="u1")))
 
-    comments += "\n".join(
-        [val for (key, val) in misc_descriptions.items() if misc_bits[key]]
-    )
+    detector_quality = {}
+    for idx in range(len(detectors)):
+        if not misc_bits[4 * idx]:
+            detector_quality[detectors[idx]] = detector_quality_opts[0]
+        elif misc_bits[4 * idx + 1]:
+            detector_quality[detectors[idx]] = detector_quality_opts[1]
+        elif misc_bits[4 * idx + 2]:
+            detector_quality[detectors[idx]] = detector_quality_opts[2]
+        elif misc_bits[4 * idx + 3]:
+            detector_quality[detectors[idx]] = detector_quality_opts[3]
 
     return {
-        "alert_type": "initial",
+        "alert_type": "retraction" if trig_id_bits[5] else "initial",
         "alert_tense": "test" if trig_id_bits[1] else "current",
         "messenger": "Neutrino",
         "id": [bin[4]],
         "trigger_time": utils.datetime_to_iso8601(bin[5], bin[6]),
-        "ra": bin[7] * 1e-4,
-        "dec": bin[8] * 1e-4,
+        "ra": None if trig_id_bits[2] else ra,
+        "dec": None if trig_id_bits[2] else dec,
         "ra_dec_error": bin[11] * 1e-4,
         "containment_probability": bin[12] * 1e-2,
         "n_events": bin[9],
         "duration": bin[13] * 1e-2,
-        "additional_info": comments if comments else None,
+        "detector_quality": detector_quality,
     }
